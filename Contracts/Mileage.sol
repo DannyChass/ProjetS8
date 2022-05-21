8851c12cde5168d464f6c4f474d98297941b5b7e
Daniel <daniel.chassagn@gmail.com>
2022-05-21 21:10:15 +0200

    Update Mileage.sol

--- Contracts/Mileage.sol
+++ Contracts/Mileage.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.5.16;
+pragma solidity ^0.8.7;
 
 contract Mileage{
 
@@ -11,7 +11,7 @@ contract Mileage{
         uint timestamp;
     }
 
-    mapping(address => MileageEntry[]) public mileageEntries;
+    mapping(bytes32 => MileageEntry[]) public mileageEntries;
     mapping(bytes32 => address) public mappedVinToKeyEntries;
     address owner;
 
@@ -28,16 +28,26 @@ contract Mileage{
     }
 
 
-    function storeMileage(uint inputMileage, uint inputTimestamp) public{
+    function storeMileage(uint inputMileage, uint inputTimestamp, bytes32 vin) public{
         address vehicleIdentity = msg.sender;
-        mileageEntries[vehicleIdentity].push(MileageEntry({
+        mileageEntries[vin].push(MileageEntry({
             mileageInMeters:inputMileage, timestamp:inputTimestamp
             }));
     }
 
-    function countEntriesOfAdress(address vehiculeIdentity) public view returns (uint counter){
-        counter = mileageEntries[vehiculeIdentity].length;
+    function countEntriesOfAdress(bytes32 vin) public  view returns (uint counter){
+        counter = mileageEntries[vin].length;
         return counter;
+    }
+    
+    function getValueKM(bytes32 vin, uint index) public view returns (uint value) {
+        value = mileageEntries[vin][index].mileageInMeters;
+        return value;
+    }
+
+    function getValueTime(bytes32 vin, uint index) public view returns (uint time) {
+        time = mileageEntries[vin][index].timestamp;
+        return time;
     }
 
     function resetVinMapping(bytes32 hashedVin) public {
@@ -45,15 +55,33 @@ contract Mileage{
         mappedVinToKeyEntries[hashedVin]= address(0x0);
     }
 
-    function resetEntriesForPublicKey(address publicKey) public{
-        require(mileageEntries[publicKey].length != 0);
-        delete mileageEntries[publicKey];
+
+    function resetEntriesForPublicKey(bytes32 vin) public{
+        require(mileageEntries[vin].length != 0);
+        delete mileageEntries[vin];
     }
 
+    //Convert a string to a uint
+    function st2num(string memory numString) public pure returns(uint val) {
+
+        bytes   memory stringBytes = bytes(numString);
+        for (uint  i =  0; i<stringBytes.length; i++) {
+            uint exp = stringBytes.length - i;
+            bytes1 ival = stringBytes[i];
+            uint8 uval = uint8(ival);
+           uint jval = uval - uint(0x30);
+   
+           val +=  (uint(jval) * (10**(exp-1)));
+        }
+      return val;
+    }
+
+    //Hash a string to a bytes32
     function getSha256(string memory str) public pure returns (bytes32) {
 
         bytes32 hash = sha256(abi.encodePacked(str));
         return hash;
         //string y    = hash;
         }
+     
 }
